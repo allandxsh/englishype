5f2e17e483cecf373cf2e320a67e1cb6c0c74164
allanxiu <allanhmc9@gmail.com>
2021-03-05 17:16:13 -0300

    first

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { FaMagento, FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { Button } from "../../globalStyles";
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubHeading,
+  FooterSubText,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinksItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+  SocialIcon
+} from "./Footer.elements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Se cadastre para receber e-mails e novidades sobre nós!
+        </FooterSubHeading>
+        <FooterSubText>Você pode cancelar a qualquer momento.</FooterSubText>
+        <Form>
+          <FormInput name="email" type="email" placeholder="example@gmail.com" />
+            <Button fontBig>Inscreva-se</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinksItems>
+            <FooterLinkTitle>Sobre Nós</FooterLinkTitle>
+            <FooterLink to='/sign-up'>Como funciona?</FooterLink>
+            <FooterLink to='/'>Suporte</FooterLink>
+            <FooterLink to='/'>Avaliações</FooterLink>
+            <FooterLink to='/'>Preços</FooterLink>
+            <FooterLink to='/'>Termos de Serviço</FooterLink>
+          </FooterLinksItems>
+
+          <FooterLinksItems>
+            <FooterLinkTitle>Contato</FooterLinkTitle>
+            <FooterLink to='/sign-up'>Como funciona?</FooterLink>
+            <FooterLink to='/'>Suporte</FooterLink>
+            <FooterLink to='/'>Avaliações</FooterLink>
+            <FooterLink to='/'>Preços</FooterLink>
+            <FooterLink to='/'>Termos de Serviço</FooterLink>
+          </FooterLinksItems>
+
+          <FooterLinksItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to='/sign-up'>Como funciona?</FooterLink>
+            <FooterLink to='/'>Suporte</FooterLink>
+            <FooterLink to='/'>Avaliações</FooterLink>
+            <FooterLink to='/'>Preços</FooterLink>
+            <FooterLink to='/'>Termos de Serviço</FooterLink>
+          </FooterLinksItems>
+
+          <FooterLinksItems>
+            <FooterLinkTitle>Mídia Social</FooterLinkTitle>
+            <FooterLink to='/sign-up'>Como funciona?</FooterLink>
+            <FooterLink to='/'>Suporte</FooterLink>
+            <FooterLink to='/'>Avaliações</FooterLink>
+            <FooterLink to='/'>Preços</FooterLink>
+            <FooterLink to='/'>Termos de Serviço</FooterLink>
+          </FooterLinksItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/" >
+            <SocialIcon>
+              ULTRA
+            </SocialIcon>
+          </SocialLogo>
+          <WebsiteRights>ENGLISH HYPE 2021</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href='/' target="_blank" aria-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href='/' target="_blank" aria-label="Instagram">
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink href='/' target="_blank" aria-label="Youtube">
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href='/' target="_blank" aria-label="Twitter">
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href='/' target="_blank" aria-label="Linkedin">
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,67 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Comece Agora',
+    description: 'Nós ajudamos você a se desenvolver!',
+    headline: 'Treine e desenvolva seu inglês com exércicios e aulas online.',
+    lightText: true,
+    topLine: 'Aulas Online',
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Image',
+    start: ''
+};
+
+export const homeObjTwo = {
+    lightBg: true,
+    primary: false,
+    imgStart: 'start',
+    lightTopLine: false,
+    lightTextDesc: false,
+    buttonLabel: 'Faça uma aula grátis',
+    description: 'Conte com nossos professores nativos para um aprendizado mais rápido, ajudando você nessa nova caminhada.',
+    headline: 'Professores Nativos',
+    lightText: false,
+    topLine: 'Conversação Online',
+    img: require('../../images/profile.jpg').default,
+    alt: 'Image',
+    start: 'true'
+};
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Confira Aqui!',
+    description: 'Nosso gráfico de aprendizado comparado a outras empresas é surpreendente.',
+    headline: 'Quer acelerar seu aprendizado? Está no lugar certo.',
+    lightText: true,
+    topLine: 'Estatísticas',
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Image',
+    start: ''
+};
+
+export const homeObjFour = {
+    lightBg: true,
+    primary: false,
+    imgStart: 'start',
+    lightTopLine: false,
+    lightTextDesc: false,
+    buttonLabel: 'Faça uma aula grátis',
+    description: 'Conte com nossos professores nativos para um aprendizado mais rápido, ajudando você nessa nova caminhada.',
+    headline: 'Professores Nativos',
+    lightText: false,
+    topLine: 'Conversação Online',
+    img: require('../../images/profile.jpg').default,
+    alt: 'Image',
+    start: 'true'
+};
+
+
+
+
